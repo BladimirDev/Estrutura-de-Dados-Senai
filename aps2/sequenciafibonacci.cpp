267a69f9b53ca700f2565fdf4fce2130be1b697e
BladimirDev <bladimir.dev.contato@gmail.com>
2026-04-29 00:15:55 -0300

    fiz a atividade da aps2 que era a sequencia fibonacci

--- aps2/sequenciafibonacci.cpp
+++ aps2/sequenciafibonacci.cpp
@@ -2,22 +2,61 @@
 //Bladimir José Villarroel Guevara
 using namespace std;
 
-int main (){
-    int pasado = 0;
-    int anterior = 1;
-    int result;
-    int selecao;
+struct No{ // Criação da struct No
+    int dado;
+    struct No *prox;
+};
 
-    cout << "Para comecar escreva a quantia de termos da série fibonacci que voce quer gerar: ";
-    cin >> selecao;
+struct No *inicio = NULL; //Criação do inicio da struct
 
-    cout << "Termo 1: " << pasado << endl;
-    cout << "Termo 2: " << anterior << endl;
+void inserirFinal(int valor){ //Função para inserir o valor no final da lista encadeada
+    struct No *novo = (struct No*) malloc(sizeof(struct No));
 
-    for (int i = 0; i < selecao ; i++){
+    novo->dado = valor;
+    novo->prox = NULL;
+    
+    if (inicio == NULL) {
+        inicio = novo;
+    }else{
+        struct No *aux = inicio;
+        while (aux->prox != NULL) {
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+    }
+}
+
+void exibirFibonacci(){ // Função para exibir todos os dados armazenados na lista encadeada
+    struct No *aux = inicio;
+
+    while (aux != NULL) {
+        cout << aux->dado;
+        if (aux->prox != NULL) {
+            cout << " -> ";
+        }
+        aux = aux->prox;
+    }
+    cout << endl;
+}
+
+int main (){ //Função principal
+    int pasado = 0, anterior = 1, result, num;
+
+    cout << "Quantos termos de fibonacci?: ";
+    cin >> num;
+
+    if (num >= 1 ) {
+        inserirFinal(0);
+    }
+    if (num >= 2) {
+        inserirFinal(1);
+    }
+
+    for (int i = 2; i < num ; i++){ //Criação e inserção dos termos na struct
         result = pasado + anterior;
-        cout << "termo " << i+3 << ":" << result << endl;
+        inserirFinal(result);
         pasado = anterior;
         anterior = result; 
     };
+    exibirFibonacci();//mostramos a série(A quiantia que é pedida antes)
 };
